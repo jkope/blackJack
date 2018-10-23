39d57b98d515632ac3177974234552b6bf76ddb8
Jake K <jake.kopenhefer@gmail.com>
2018-10-22 20:11:58 -0600

    blackJack

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-
 let deck = {};
-
-$('deal').click(dealCards)
-
 let request = new XMLHttpRequest();
-request.onreadystatechange = () => {
+
+function newGame(){
     if (request.readyState === XMLHttpRequest.DONE) {
         let response = JSON.parse(request.response);
         if (response.success) {
@@ -14,6 +11,8 @@ request.onreadystatechange = () => {
         dealCards();
     }
 };
+
+
 
 request.open("GET", "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6");
 request.send();
@@ -26,14 +25,52 @@ function dealCards() {
             if (response.success) {
                 document.getElementById('card1').src = response.cards[0].image
                 document.getElementById('card2').src = response.cards[1].image
-
+                document.getElementById('card3').src = response.cards[2].image
+                document.getElementById('card4').src = response.cards[3].image
             }
         }
     };
 
-    cardRequest.open("GET", `https://deckofcardsapi.com/api/deck/${deck.id}/draw/?count=2`);
+    cardRequest.open("GET", `https://deckofcardsapi.com/api/deck/${deck.id}/draw/?count=4`);
     cardRequest.send();
 }
 
 
+function singleCard1() {
+    let cardRequest = new XMLHttpRequest();
+    cardRequest.onreadystatechange = () => {
+        if (cardRequest.readyState === XMLHttpRequest.DONE) {
+            let response = JSON.parse(cardRequest.response);
+            if (response.success) {
+                    $('#ply1').append(`<img src="${response.cards[0].image}" alt</img></img>`)
+            }
+        }
+    };
+
+    cardRequest.open("GET", `https://deckofcardsapi.com/api/deck/${deck.id}/draw/?count=1`);
+    cardRequest.send();
+}
+
+function singleCard2() {
+    let cardRequest = new XMLHttpRequest();
+    cardRequest.onreadystatechange = () => {
+        if (cardRequest.readyState === XMLHttpRequest.DONE) {
+            let response = JSON.parse(cardRequest.response);
+            if (response.success) {
+                $('#ply2').append(`<img src="${response.cards[0].image}" alt</img></img>`)
+            }
+        }
+    };
+
+    cardRequest.open("GET", `https://deckofcardsapi.com/api/deck/${deck.id}/draw/?count=1`);
+    cardRequest.send();
+}
+
+function clearGame() {
+    location.reload();
+}
+$('#newGame').click(newGame);
+$('#clearGame').click(()=>{location.reload();});
+$('#hit1').click(singleCard1);
+$('#hit2').click(singleCard2);
 
